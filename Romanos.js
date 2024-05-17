ac8208fad2d88ed73e0b441370e04dc2fe39de8e
TechOtavio <150319160+TechOtavio@users.noreply.github.com>
2024-05-16 23:09:30 -0300

    Update Romanos.js

--- Romanos.js
+++ Romanos.js
@@ -1,70 +1,28 @@
-numerosEmRomanos = [];
-function transformador(numero) {
-    while (numero >= 1000) {
-      numerosEmRomanos.push("M");
-      numero -= 1000;
-    }
+//Atualização
+function integerToRoman(number){
+  let numerosEmRomanos = "";
 
-    while (numero >= 900) {
-      numerosEmRomanos.push("CM")
-      numero -= 900;
-    }
-
-    while (numero >= 500) {
-      numerosEmRomanos.push("D");
-      numero -= 500;
+  const romanToNumber = [
+      ["M", 1000],
+      ["CM", 900],
+      ["D", 500],
+      ["CD", 400],
+      ["C", 100],
+      ["XC", 90],
+      ["L", 50],
+      ["XL", 40],
+      ["X", 10],
+      ["IX", 9],
+      ["V", 5],
+      ["IV", 4],
+      ["I", 1]
+  ]
+  for (let i = 0; i < romanToNumber.length; i++){
+      while(number >= romanToNumber[i][1]){
+          numerosEmRomanos += romanToNumber[i][0];
+          number -= romanToNumber;
       }
-    
-    while (numero >= 400) {
-      numerosEmRomanos.push("CD");
-      numero -= 400;
-    }
-
-    while (numero >= 100) {
-      numerosEmRomanos.push("C");
-      numero -= 100;
-    }
-
-    while (numero >= 90) {
-      numerosEmRomanos.push("XC");
-      numero -= 90;
-    }
-
-    while (numero >= 50) {
-      numerosEmRomanos.push("L");
-      numero -= 50;
-    }
-
-    while (numero >= 40) {
-      numerosEmRomanos.push("XL");
-      numero -= 40;
-    }
-
-    while (numero >= 10) {
-      numerosEmRomanos.push("X");
-      numero -= 10;
-    }
-
-    while (numero == 9) {
-      numerosEmRomanos.push("IX");
-      numero -= 9;
-    }
-
-    while (numero >= 5) {
-      numerosEmRomanos.push("V");
-      numero -= 5;
-    }
-
-    while (numero == 4) {
-      numerosEmRomanos.push("IV");
-      numero -= 4;
-    }
-
-    while (numero >= 1) {
-      numerosEmRomanos.push("I");
-      numero -= 1;
-    }
-  return numerosEmRomanos.join("");
+  }
+  numerosEmRomanos(2534);
+  return numerosEmRomanos;
 }
-
-console.log(transformador());
